f444d9d57659ab45427a664bd05370fedc036cad
swordarrow2 <2856986197@qq.com>
2019-05-08 23:23:58 +0800

    javabean

---
+++ v2/app/src/main/java/com/meng/bilibilihelper/javaBean/liveAddress/LiveAddress.java
@@ -0,0 +1,8 @@
+package com.meng.bilibilihelper.javaBean.liveAddress;
+
+public class LiveAddress {
+    public int code = 0;
+    public String msg = "";
+    public String message = "";
+    public LiveAddressData data = new LiveAddressData();
+}
---
+++ v2/app/src/main/java/com/meng/bilibilihelper/javaBean/liveAddress/QualityDescription.java
@@ -0,0 +1,6 @@
+package com.meng.bilibilihelper.javaBean.liveAddress;
+
+public class QualityDescription {
+    public int qn = 0;
+    public String desc = "";
+}

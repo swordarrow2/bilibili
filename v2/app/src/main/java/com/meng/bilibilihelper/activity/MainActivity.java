bbb25f2daf802471aba9c4e56f35b82e3e15c6be
swordarrow2 <2856986197@qq.com>
2019-05-13 22:57:16 +0800

    评论视频测试

--- v2/app/src/main/java/com/meng/bilibilihelper/activity/MainActivity.java
+++ v2/app/src/main/java/com/meng/bilibilihelper/activity/MainActivity.java
@@ -23,6 +23,9 @@ import com.meng.bilibilihelper.libAndHelper.SharedPreferenceHelper;
 import com.meng.bilibilihelper.materialDesign.*;
 
 import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.*;
 
 import org.jsoup.*;
@@ -53,6 +56,7 @@ public class MainActivity extends Activity {
     public SendDanmakuFragment sendDanmakuFragment;
     public SendHotStripFragment sendHotStripFragment;
     public GiftFragment giftFragment;
+    public ReplayVidoeFragment replayVidoeFragment;
 
     public FragmentManager fragmentManager;
     public RelativeLayout relativeLayout;
@@ -201,7 +205,7 @@ public class MainActivity extends Activity {
         mDrawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, new String[]{
                 "首页(大概)", "信息", "添加账号", "管理账号",
                 "奶", "挂机", "发送弹幕", "发送礼物",
-                "签到", "设置", "退出"
+                "签到","j", "设置", "退出"
         }));
         mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -243,6 +247,9 @@ public class MainActivity extends Activity {
                     case "挂机":
                         initGuajiFragment(true);
                         break;
+                    case "j":
+                        initReplyVideoFragment(true);
+                        break;
                     case "签到":
                         initSignFragment(true);
                         break;
@@ -281,6 +288,7 @@ public class MainActivity extends Activity {
         initNaiFragment(false);
         initHotStripFragment(false);
         initGiftFragment(false);
+        initReplyVideoFragment(false);
         initMainFragment(true);
     }
 
@@ -414,6 +422,19 @@ public class MainActivity extends Activity {
         transactionBusR.commit();
     }
 
+    private void initReplyVideoFragment(boolean showNow) {
+        FragmentTransaction transactionBusR = fragmentManager.beginTransaction();
+        if (replayVidoeFragment == null) {
+            replayVidoeFragment = new ReplayVidoeFragment();
+            transactionBusR.add(R.id.main_activityLinearLayout, replayVidoeFragment);
+        }
+        hideFragment(transactionBusR);
+        if (showNow) {
+            transactionBusR.show(replayVidoeFragment);
+        }
+        transactionBusR.commit();
+    }
+
 
     public void hideFragment(FragmentTransaction transaction) {
         Fragment fs[] = {
@@ -426,7 +447,8 @@ public class MainActivity extends Activity {
                 settingsFragment,
                 personInfoFragment,
                 sendDanmakuFragment,
-                sendHotStripFragment
+                sendHotStripFragment,
+                replayVidoeFragment
         };
         for (Fragment f : fs) {
             if (f != null) {
@@ -537,6 +559,63 @@ public class MainActivity extends Activity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void sendDataPost(String url,String referer,String origin, String cookie,HashMap<String,String> data) throws IOException {
+        URL postUrl = new URL("http://api.live.bilibili.com/gift/v2/gift/send");
+        StringBuilder content = new StringBuilder();//要发出的数据
+        // 打开连接
+        HttpURLConnection connection = (HttpURLConnection) postUrl.openConnection();
+        // 设置是否向connection输出，因为这个是post请求，参数要放在http正文内，因此需要设为true
+        connection.setDoOutput(true);
+        connection.setDoInput(true);
+        connection.setRequestMethod("POST");
+        //	 Post请求不能使用缓存
+        connection.setUseCaches(false);
+        connection.setInstanceFollowRedirects(true);
+        connection.setRequestProperty("Host", "api.live.bilibili.com");
+        connection.setRequestProperty("Connection", "keep-alive");
+        connection.setRequestProperty("Accept", "application/json, text/javascript, */*; q=0.01");
+        connection.setRequestProperty("Origin", origin);
+        connection.setRequestProperty("User-Agent", MainActivity.instence.userAgent);
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        connection.setRequestProperty("Referer", referer);
+        connection.setRequestProperty("Accept-Encoding", "gzip, deflate, br");
+        connection.setRequestProperty("Accept-Language", "zh-CN,zh;q=0.8");
+        connection.setRequestProperty("cookie", cookie);
+        for (String key:data.keySet()){
+            content.append(key).append("=").append(data.get(key)).append("&");
+        }
+        content = new StringBuilder(content.substring(0, content.lastIndexOf("&") - 1));
+        connection.setRequestProperty("Content-Length", String.valueOf(content.length()));
+        // 连接,从postUrl.openConnection()至此的配置必须要在 connect之前完成
+        // 要注意的是connection.getOutputStream会隐含的进行 connect
+        connection.connect();
+        DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+        out.writeBytes(content.toString());
+        out.flush();
+        out.close();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String line;
+        StringBuilder s = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            s.append(line);
+        }
+        String ss = s.toString();
+        MainActivity.instence.showToast(ss);
+        reader.close();
+        connection.disconnect();
+    }
+
+    public String encode(String url) {
+        try {
+            return URLEncoder.encode(url, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            return "Issue while encoding" + e.getMessage();
+        }
+    }
+    public String getCsrf(String cookie){
+        return MainActivity.instence.cookieToMap(cookie).get("bili_jct");
     }
 
     @Override
---
+++ v2/app/src/main/java/com/meng/bilibilihelper/fragment/ReplayVidoeFragment.java
@@ -0,0 +1,129 @@
+package com.meng.bilibilihelper.fragment;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.gson.Gson;
+import com.meng.bilibilihelper.R;
+import com.meng.bilibilihelper.activity.LiveWebActivity;
+import com.meng.bilibilihelper.activity.MainActivity;
+import com.meng.bilibilihelper.adapters.ListWithImageSwitchAdapter;
+import com.meng.bilibilihelper.javaBean.LoginInfoPeople;
+import com.meng.bilibilihelper.javaBean.SendDanmakuReturnedData;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class ReplayVidoeFragment extends Fragment {
+    ListView listview;
+    Button btn;
+    EditText et;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.send_danmaku_custom, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        listview = (ListView) view.findViewById(R.id.send_danmaku_customListView);
+        btn = (Button) view.findViewById(R.id.send_danmaku_customButton);
+        et = (EditText) view.findViewById(R.id.send_danmaku_customEditText);
+
+        listview.setAdapter(new ListWithImageSwitchAdapter(MainActivity.instence, MainActivity.instence.loginInfo.loginInfoPeople));
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View p1) {
+                new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        ListWithImageSwitchAdapter cda = (ListWithImageSwitchAdapter) listview.getAdapter();
+
+                        for (int i = 0; i < cda.getCount(); ++i) {
+                            if (cda.getChecked(i)) {
+                                try {
+                                    sendReplyData("发发发",((LoginInfoPeople)cda.getItem(i)).cookie,String.valueOf(39594559));
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                }).start();
+            }
+        });
+    }
+
+    public void sendReplyData(String msg, String cookie, String AID) throws IOException {
+        URL postUrl = new URL("https://api.bilibili.com/x/v2/reply/add");
+        // 打开连接
+        HttpURLConnection connection = (HttpURLConnection) postUrl.openConnection();
+        // 设置是否向connection输出，因为这个是post请求，参数要放在http正文内，因此需要设为true
+        connection.setDoOutput(true);
+        connection.setDoInput(true);
+        connection.setRequestMethod("POST");
+        //	 Post请求不能使用缓存
+        connection.setUseCaches(false);
+        connection.setInstanceFollowRedirects(true);
+        connection.setRequestProperty("Host", "api.bilibili.com");
+        connection.setRequestProperty("Connection", "keep-alive");
+        connection.setRequestProperty("Accept", "application/json, text/javascript, */*; q=0.01");
+        connection.setRequestProperty("Origin", "https://www.bilibili.com");
+        connection.setRequestProperty("User-Agent", MainActivity.instence.userAgent);
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        connection.setRequestProperty("Referer", "https://www.bilibili.com/video/av" + AID);
+        connection.setRequestProperty("Accept-Encoding", "gzip, deflate, br");
+        connection.setRequestProperty("Accept-Language", "zh-CN,zh;q=0.8");
+        connection.setRequestProperty("cookie", cookie);
+        String content = "oid=" + AID +
+                "&type=1" +
+                "&message=" + encode(msg) +
+                "&jsonp=jsonp" +
+                "&csrf=" + MainActivity.instence.cookieToMap(cookie).get("bili_jct");
+        connection.setRequestProperty("Content-Length", String.valueOf(content.length()));
+        // 连接,从postUrl.openConnection()至此的配置必须要在 connect之前完成
+        // 要注意的是connection.getOutputStream会隐含的进行 connect
+        connection.connect();
+        DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+        out.writeBytes(content);
+        out.flush();
+        out.close();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String line;
+        StringBuilder s = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            s.append(line);
+        }
+        final String ss = s.toString();
+        reader.close();
+        connection.disconnect();
+
+    }
+
+    public String encode(String url) {
+        try {
+            return URLEncoder.encode(url, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            return "Issue while encoding" + e.getMessage();
+        }
+    }
+}

d802d2840c5a7b048d9069e9f2b5b5770cdf626b
swordarrow2 <2856986197@qq.com>
2019-06-16 14:14:52 +0800

    改房间名

--- v2/app/src/main/java/com/meng/bilibilihelper/libAndHelper/MengLiveControl.java
+++ v2/app/src/main/java/com/meng/bilibilihelper/libAndHelper/MengLiveControl.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import com.google.gson.Gson;
@@ -23,15 +24,19 @@ import java.util.Map;
 
 public class MengLiveControl extends LinearLayout {
 
-    private Button btn;
+    private Button btnStart;
+    private Button btnRename;
+    private EditText newName;
     private LinearLayout ll;
-	private MengTextview m1;
-	private MengTextview m2;
-	
+    private MengTextview m1;
+    private MengTextview m2;
+
     public MengLiveControl(final Context context) {
         super(context);
         LayoutInflater.from(context).inflate(R.layout.meng_live_control, this);
-        btn = (Button) findViewById(R.id.btn);
+        btnStart = (Button) findViewById(R.id.btn_start);
+        btnRename = (Button) findViewById(R.id.btn_rename);
+        newName = (EditText) findViewById(R.id.et_new_name);
         ll = (LinearLayout) findViewById(R.id.linearlayout);
         new Thread(new Runnable() {
             @Override
@@ -42,16 +47,16 @@ public class MengLiveControl extends LinearLayout {
                     ((Activity) context).runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            btn.setText(sjb.data.liveStatus == 0 ? "开始直播" : "关闭直播");
+                            btnStart.setText(sjb.data.liveStatus == 0 ? "开始直播" : "关闭直播");
                         }
                     });
                 }
             }
         }).start();
-        btn.setOnClickListener(new OnClickListener() {
+        btnStart.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (btn.getText().toString().equals("开始直播")) {
+                if (btnStart.getText().toString().equals("开始直播")) {
                     new Thread(new Runnable() {
                         @Override
                         public void run() {
@@ -67,14 +72,14 @@ public class MengLiveControl extends LinearLayout {
                                 } catch (IOException e) {
                                     e.printStackTrace();
                                 }
-                                 m1 = new MengTextview(context, "", rtmp.get("addr").getAsString());
-                               m2 = new MengTextview(context, "", rtmp.get("code").getAsString());
+                                m1 = new MengTextview(context, "", rtmp.get("addr").getAsString());
+                                m2 = new MengTextview(context, "", rtmp.get("code").getAsString());
                                 ((Activity) context).runOnUiThread(new Runnable() {
                                     @Override
                                     public void run() {
                                         ll.addView(m1);
                                         ll.addView(m2);
-                                        btn.setText("关闭直播");
+                                        btnStart.setText("关闭直播");
                                     }
                                 });
                             }
@@ -96,12 +101,38 @@ public class MengLiveControl extends LinearLayout {
                                 public void run() {
                                     ll.removeView(m1);
                                     ll.removeView(m2);
-                                    btn.setText("开始直播");
+                                    btnStart.setText("开始直播");
                                 }
                             });
                         }
                     }).start();
                 }
+            }
+        });
+        btnRename.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String name = newName.getText().toString();
+                if (name.equals("")) {
+                    MainActivity.instence.showToast("房间名不能为空");
+                    return;
+                }
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String mainUID = SharedPreferenceHelper.getValue("mainAccount", "");
+                        if (!mainUID.equals("")) {
+                            String cookie = MainActivity.instence.getCookie(Long.parseLong(mainUID));
+                            UserSpaceToLive sjb = new Gson().fromJson(MainActivity.instence.getSourceCode("https://api.live.bilibili.com/room/v1/Room/getRoomInfoOld?mid=" + mainUID), UserSpaceToLive.class);
+                            try {
+                                rename(sjb.data.roomid, cookie, name);
+                            } catch (IOException e) {
+                                MainActivity.instence.showToast(e.toString());
+                            }
+                        }
+                    }
+                }).start();
+                ;
             }
         });
     }
@@ -152,6 +183,34 @@ public class MengLiveControl extends LinearLayout {
                 .cookies(MainActivity.instence.cookieToMap(cookie))
                 .method(Connection.Method.POST)
                 .data("room_id", String.valueOf(roomID))
+                .data("csrf_token", csrf)
+                .data("csrf", csrf);
+        Connection.Response response = connection.execute();
+        if (response.statusCode() != 200) {
+            MainActivity.instence.showToast(String.valueOf(response.statusCode()));
+        }
+        JsonParser parser = new JsonParser();
+        JsonObject obj = parser.parse(response.body()).getAsJsonObject();
+        MainActivity.instence.showToast(obj.get("message").getAsString());
+    }
+
+    public void rename(int roomID, String cookie, String newName) throws IOException {
+        Connection connection = Jsoup.connect("https://api.live.bilibili.com/room/v1/Room/update");
+        String csrf = MainActivity.instence.cookieToMap(cookie).get("bili_jct");
+        Map<String, String> liveHead = new HashMap<>();
+        liveHead.put("Host", "api.live.bilibili.com");
+        liveHead.put("Accept", "application/json, text/javascript, */*; q=0.01");
+        liveHead.put("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        liveHead.put("Connection", "keep-alive");
+        liveHead.put("Origin", "https://link.bilibili.com");
+        connection.userAgent(MainActivity.instence.userAgent)
+                .headers(liveHead)
+                .ignoreContentType(true)
+                .referrer("https://link.bilibili.com/p/center/index")
+                .cookies(MainActivity.instence.cookieToMap(cookie))
+                .method(Connection.Method.POST)
+                .data("room_id", String.valueOf(roomID))
+                .data("title", newName)
                 .data("csrf_token", csrf)
                 .data("csrf", csrf);
         Connection.Response response = connection.execute();

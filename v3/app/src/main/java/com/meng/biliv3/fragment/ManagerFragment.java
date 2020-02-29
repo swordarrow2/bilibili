be3c2ae52afb76b7527175540a527e229f7a9827
c59862143 <8255053@qq.com>
2020-02-29 20:38:22 +0800

    管理账号界面修改

--- v3/app/src/main/java/com/meng/biliv3/fragment/ManagerFragment.java
+++ v3/app/src/main/java/com/meng/biliv3/fragment/ManagerFragment.java
@@ -16,6 +16,8 @@ import android.view.View.OnClickListener;
 
 public class ManagerFragment extends Fragment {
 
+	private AlertDialog selectOpDialog;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.account_manager, container, false);
@@ -39,31 +41,45 @@ public class ManagerFragment extends Fragment {
         list.setOnItemLongClickListener(new OnItemLongClickListener() {
 
 				@Override
-				public boolean onItemLongClick(final AdapterView<?> p11, View p2, final int p3, long p4) {
+				public boolean onItemLongClick(final AdapterView<?> p11, View p2, final int position, long p4) {
 
-					new AlertDialog.Builder(getActivity())
-                        .setTitle("选择操作")
-                        .setPositiveButton("删除", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface p11, int p2) {
-                                new AlertDialog.Builder(getActivity())
-									.setTitle("确定删除吗")
-									.setPositiveButton("确定", new DialogInterface.OnClickListener() {
-										@Override
-										public void onClick(DialogInterface p1, int p2) {
-											MainActivity.instance.loginAccounts.remove(p3);
-											MainActivity.instance.saveConfig();
-											MainActivity.instance.mainAccountAdapter.notifyDataSetChanged();
-										}
-									}).setNegativeButton("取消", null).show();
-                            }
-                        }).setNegativeButton("更新登录状态", new DialogInterface.OnClickListener() {
+					ListView lvSelectOp = new ListView(getActivity());
+					lvSelectOp.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, android.R.id.text1, new String[]{ "上移", "下移", "更新登录状态", "删除"}));
+					lvSelectOp.setOnItemClickListener(new OnItemClickListener() {
+
 							@Override
-							public void onClick(DialogInterface dialog, int which) {
-								startActivity(new Intent(getActivity(), Login.class));
+							public void onItemClick(AdapterView<?> p1, View p2, int selectOp, long p4) {
+								String sel=(String) p1.getAdapter().getItem(selectOp);
+								if (sel.equals("删除")) {
+									new AlertDialog.Builder(getActivity()).setTitle("确定删除" + MainActivity.instance.loginAccounts.get(position).name + "吗").setPositiveButton("确定", new DialogInterface.OnClickListener() {
+											@Override
+											public void onClick(DialogInterface p1, int p2) {
+												MainActivity.instance.loginAccounts.remove(position);
+											}
+										}).setNegativeButton("取消", null).show();
+								} else if (sel.equals("上移")) {
+									if (position > 0) {
+										MainActivity.instance.loginAccounts.add(position - 1, MainActivity.instance.loginAccounts.remove(position));
+									} else {
+										MainActivity.instance.showToast("已经是最上面了");
+									}
+								} else if (sel.equals("下移")) {
+									if (position < MainActivity.instance.loginAccounts.size() - 1) {
+										MainActivity.instance.loginAccounts.add(position + 1, MainActivity.instance.loginAccounts.remove(position));
+									} else {
+										MainActivity.instance.showToast("已经是最下面了");
+									}
+								} else if (sel.equals("更新登录状态")) {
+									startActivity(new Intent(getActivity(), Login.class));
+								}
+								MainActivity.instance.saveConfig();
+								MainActivity.instance.mainAccountAdapter.notifyDataSetChanged();
+								BaseIdFragment.createSpinnerList();
+								selectOpDialog.cancel();
+								selectOpDialog = null;
 							}
-						}).show();
-
+						});
+					selectOpDialog = new AlertDialog.Builder(getActivity()).setView(lvSelectOp).setTitle("选择操作").setNegativeButton("返回", null).show();
 					return true;
 				}
 			});

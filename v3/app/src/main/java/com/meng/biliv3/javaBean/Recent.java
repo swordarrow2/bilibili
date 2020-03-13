3bc395fcca2c3cf657888de91893225f9da645c8
swordarrow2 <8255053@qq.com>
2020-03-13 20:07:04 +0800

    v4只保留了需要的代码

---
+++ v3/app/src/main/java/com/meng/biliv3/javaBean/Recent.java
@@ -0,0 +1,13 @@
+package com.meng.biliv3.javaBean;
+
+public class Recent {
+	public String type;
+	public long id;
+	public String name;
+
+	public Recent(String type, long id, String name) {
+		this.type = type;
+		this.id = id;
+		this.name = name;
+	}
+}
